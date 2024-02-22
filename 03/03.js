5f74f08c82e2d0c9bc44180f04bffca207e8ac93
yuyeong00 <whslslrhal@gmail.com>
2024-02-22 12:51:22 +0900

    js

--- 03/03.js
+++ 03/03.js
@@ -1,1 +1,15 @@
-alert("안녕")
+// alert("안녕")
+
+// 버튼1 누르면 메세지영역에 글씨 나오게 하기 (로그인버튼)
+// 함수 만들기
+function handleClick(n) {
+    //메시지영역 가져오기
+    // document.getElementById("msgArea").innerHTML = "안녕하세요";
+    //(메시지영역div집고). innerHTML속성
+    
+    // //메시지영역 가져오기2 //HTML이라 스타일태그 적용가능
+    // document.querySelector("#msgArea").innerHTML = "<h2>버튼 " + n + " 눌러졌습니다.</h2>"
+
+    
+    document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n} 눌러졌습니다.</h2>`; //백틱 문자열
+}
